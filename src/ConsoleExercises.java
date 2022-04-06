17660d6e38a66f7c7c8e71fc89a90a69c543c671
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-06 13:11:23 -0500

    first part console exercises done

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,17 @@
+public class ConsoleExercises {
+
+    public static void main(String[] args){
+
+        double pi = 3.14159;
+
+        String approxPI = "The value of pi is approximately, ";
+
+        System.out.format(approxPI + "%3.2f%n", pi);
+
+
+
+
+
+
+    }
+}

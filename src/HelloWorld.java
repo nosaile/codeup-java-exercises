a57ee952ba17b6ceec1949b93e3040231a4e43a7
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-06 11:57:46 -0500

    completed syntax types variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,47 @@ public class HelloWorld {
 
     public static void main(String[] args){
         System.out.println("Hello, World!");
+
+        int myFavorteNumber = 41;
+
+        System.out.println(myFavorteNumber);
+
+        String myString = "3.14159";
+
+        System.out.println(myString);
+
+        float myNumber = 3.14f;
+
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        System.out.println(x += 5);
+
+//        int x = 3;
+//        int y = 4;
+//
+//        System.out.println(y * x);
+
+        int x = 10;
+        int y = 2;
+
+        System.out.println(x /= y);
+        System.out.println(y - x);
+
     }
 
 

8588070ccdb4f9961838ad816c5f0c5dd83d22e6
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-06 11:37:18 -0500

    syntax types and variables

---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,37 @@
+public class SyntaxTypesVariables {
+
+    public static void main(String[] args) {
+
+        //JAVA VARIABLE DECLARATION
+        //DATATYPE --> NAME --> VALUE
+
+//        console.log() is now sout tab or -->  System.out.println();
+
+        byte daysOfMonth = 31;
+
+        short employees = 10000;
+
+        int userID = 123456789;
+
+        long circMilkyWay = 1234567891234567891L;
+
+        float roundedPI = 3.4F;
+
+        double someBigDecimal = 3.456789;
+
+        char yesNo = 'Y';
+
+        char noYes = 'N';
+
+        boolean orTrue = true;
+
+        boolean orFalse = false;
+
+        String myString = "Some string";
+
+        String someQuote = "\"Some Quote\" - George Washington";
+
+
+
+    }
+}

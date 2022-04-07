e03111f7757608fb2149d3e8c7b020ac7257c9a6
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-07 16:50:27 -0500

    bob exercise for strings complete

---
+++ src/Bob.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean continueRunningConvo;
+        do {
+            System.out.println("Bob approaches you, what will you say to him....");
+            String userResponse = scanner.nextLine();
+            continueRunningConvo = true;
+            if (userResponse.endsWith("?")) {
+                System.out.println("Bob says, \"Suuuuuure...\"");
+            } else if (userResponse.endsWith("!")) {
+                System.out.println("Bob says, \"Whoa man chilllll...\"");
+            } else if (userResponse.equals("")) {
+                System.out.println("Bob says, \"Fine, be that way...\"");
+            } else {
+                System.out.println("Bob says, \"Whatever bro...\"");
+            }
+
+
+        } while (true);
+    }
+}

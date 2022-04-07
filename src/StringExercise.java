0fe3716ee88a28d7f570774d682152a8715f781a
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-07 16:38:21 -0500

    string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,34 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String noEducation = "We don't need no education\n";
+        noEducation = noEducation +  "We don't need no thought control";
+        System.out.println(noEducation);
+
+
+        String checkThisOut = "Check \"this\" out!,";
+        checkThisOut = checkThisOut + " \"s inside of \"s!";
+        System.out.println(checkThisOut);
+
+
+        String inWindows = "In windows,";
+        inWindows = inWindows + " the main drive is usually C:\\";
+        System.out.println(inWindows);
+
+        String doBackslash = "I can do backslashes \\,";
+        doBackslash = doBackslash + " double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(doBackslash);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

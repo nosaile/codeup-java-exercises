4afd73d14c02706ebb41dae78b8969dc8d19db1e
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-11 10:15:57 -0500

    made the series of events more user friendly

--- src/HighLow.java
+++ src/HighLow.java
@@ -7,23 +7,22 @@ public class HighLow {
 
         int result = (int) (Math.random() * 100 + 1);
 
-
+        System.out.println("Please pick a number between 1 and 100...");
         for (int i = 1; i < 11; i++) {
-            System.out.println("Please pick a number between 1 and 100...");
             int userInput = scanner.nextInt();
 
             if (userInput > result) {
-                System.out.print("Number is Lower than what you guessed...");
+                System.out.print("Number is Lower than what you guessed...please choose again...");
 
             } else if (userInput < result) {
-                System.out.print("Number is higher than what you guessed...");
+                System.out.print("Number is higher than what you guessed...please choose again...");
 
-            } else if (userInput == result){
-                System.out.print("DING DING DING WE HAVE A WINNER!!!");
+            } else {
+                System.out.print("DING DING DING WE HAVE A WINNER!!! "+ result + " was the correct number!");
                 break;
             }
 
-            System.out.println("This was your: " + i + " attempt.");
+            System.out.println("This was attempt number: " + i + " out of 10.");
 
 
         }

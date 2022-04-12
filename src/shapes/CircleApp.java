4698d080f98feac27837d8944589d4840d7055d0
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-12 13:39:29 -0500

    working through the circles portion oob

---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,13 @@
+package shapes;
+
+public class CircleApp {
+
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(5);
+        System.out.println(circle1.getArea());
+        System.out.println(circle1.getCircumference());
+
+    }
+
+
+}

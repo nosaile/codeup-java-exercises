ad570fea0804fcdba90d278239fb233ddfee0f62
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-13 16:58:48 -0500

    done

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,8 +1,25 @@
 package shapes;
 
-public class ShapesTest extends Rectangle{
+public class ShapesTest extends Rectangle {
 
+    public static void main(String[] args) {
+        Rectangle box1 = new Rectangle();
+        box1.length = 5;
+        box1.width = 4;
+        System.out.println(box1.getPerimeter(box1.length, box1.width));
+        System.out.println(box1.getArea(box1.length, box1.width));
 
+        Rectangle box2 = new Square();
+        System.out.println(((Square) box2).side(5));
+        System.out.println(box2.getPerimeter(box2.length, box2.width));
+        System.out.println(box2.getArea(box2.length, box2.width));
+
+        Square square = new Square();
+        System.out.println(square.side(3));
+        System.out.println(square.getArea(square.length));
+        System.out.println(square.getPerimeter(square.length));
+
+    }
 
 
 
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,12 +1,29 @@
 package shapes;
 
-public class Square extends Rectangle{
-
-    public String side(int theSide){
+public class Square extends Rectangle {
+    public String side(int theSide) {
+        length = theSide;
+        width = theSide;
         int perim = getPerimeter(theSide, theSide);
         int area = getArea(theSide, theSide);
-        return "The squares perimeter is "+ perim + " and the area is " + area + " based on the integer passed...";
+        return "The squares perimeter is " + perim + " and the area is " + area + " based on the integer passed...";
+
+
     }
+    public int getPerimeter(int theSide){
+        length = theSide;
+        width = theSide;
+
+        return 4 * theSide;
+    }
+
+    public int getArea(int theSide){
+        length = theSide;
+        width = theSide;
+
+        return theSide * theSide;
+    }
+
 
     public static void main(String[] args) {
         Square newSquare = new Square();

443cd77c6f7ed0a81ab49c1c12c3155671343662
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-14 10:23:11 -0500

    removed old code

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -6,24 +6,7 @@ public class Rectangle {
     protected int width;
 
 
-//    public int getPerimeter(int length, int width){
-//        int pL = (length * 2);
-//        int pW =(width * 2);
-//
-//        return pL + pW;
-//    }
-//
-//    public int getArea(int length, int width){
-//        return length * width;
-//    }
-//
-//
-//
-//    public static void main(String[] args) {
-//        Rectangle rectangle1 = new Rectangle();
-//        System.out.println(rectangle1.getPerimeter(5, 2) + " is the perimeter.");
-//        System.out.println(rectangle1.getArea(5,2) + " is the area");
-//    }
+
 
     public Rectangle(int length, int width) {
         this.length = length;
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -11,35 +11,6 @@ public class Square extends Rectangle {
 
     }
 
-
-//    public String side(int theSide) {
-//        length = theSide;
-//        width = theSide;
-//        int perim = getPerimeter(theSide, theSide);
-//        int area = getArea(theSide, theSide);
-//        return "The squares perimeter is " + perim + " and the area is " + area + " based on the integer passed...";
-//
-//
-//    }
-//    public int getPerimeter(int theSide){
-//
-//
-//
-//        return 4 * theSide;
-//    }
-//
-//    public int getArea(int theSide){
-//
-//
-//        return theSide * theSide;
-//    }
-//
-//
-//    public static void main(String[] args) {
-//        Square newSquare = new Square();
-//        System.out.println(newSquare.side(5));
-//
-//    }
 @Override
     public int getArea() {
         return super.length * super.length;

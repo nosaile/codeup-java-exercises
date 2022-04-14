7946e48d1b11eb9d16c6dfa8c450ffa18d9ca69e
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-14 11:00:57 -0500

    removed unneccessary code

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,9 +1,7 @@
 package shapes;
 
-public class ShapesTest extends Rectangle {
-    public ShapesTest(int length, int width) {
-        super(length, width);
-    }
+public class ShapesTest {
+
 
     public static void main(String[] args) {
         Rectangle box1 = new Rectangle(5,4);

21ba39e21a17160b3236ef463e403d510c392a1d
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-14 14:43:17 -0500

    interfaces and abstract

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -5,6 +5,7 @@ public class ShapesTest {
 
     public static void main(String[] args) {
         Measurable myShape;
+        Measurable myShape2;
 //        Rectangle box1 = new Rectangle(5,4);
 //        System.out.println(box1.getPerimeter());
 //        System.out.println(box1.getArea());
@@ -20,13 +21,20 @@ public class ShapesTest {
 //        System.out.println(rec1.getPerimeter());
 //        System.out.println(rec1.getArea());
 //
-        Square sqr1 = new Square(5);
-        System.out.println(sqr1.getPerimeter());
-        System.out.println(sqr1.getArea());
 
-        Rectangle rec1 = new Rectangle(3,4);
-        System.out.println(rec1.setLength());
-        System.out.println(rec1.setWidth());
+        myShape = new Rectangle(3,4);
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
+
+        myShape2 = new Square(5);
+        System.out.println(myShape2.getArea());
+        System.out.println(myShape2.getPerimeter());
+
+
+
+
+
+
 
 
 

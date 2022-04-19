c23cc9bed0f5143328f16adbf727425fbb2ff5df
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-19 11:59:45 -0500

    refactoring to a switch case

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -6,14 +6,16 @@ public class MoviesApplication {
 
 public static void filterMovie(){
     Input userResponse = new Input();
-    int choice;
+    int choice = 0;
     Movie[] newMovie = MoviesArray.findAll();
+    boolean continueToRun = true;
     do {
         System.out.println("Please select one of the following options...\n1: view all movies\n2: view animated movies\n3: view drama movies\n4: view horror movies\n5: view sci-fi movies\n6: view musical movies\n7: exit");
         choice = userResponse.getInt("Enter a choice...");
         if (choice == 1) {
             for (int i = 0; i < newMovie.length; i++) {
                 System.out.println(newMovie[i].getName() + " --- " + newMovie[i].getCategory());
+
             }
         } else if (choice == 2) {
             for (int i = 0; i < newMovie.length; i++) {
@@ -52,6 +54,64 @@ public static void filterMovie(){
 }
 
 
+
+//doesnt work
+//    public static void filterMovie(){
+//        Input userResponse = new Input();
+//        int choice;
+//        Movie[] newMovie = MoviesArray.findAll();
+//        boolean continueToRun = true;
+//        do {
+//            System.out.println("Please select one of the following options...\n1: view all movies\n2: view animated movies\n3: view drama movies\n4: view horror movies\n5: view sci-fi movies\n6: view musical movies\n7: exit");
+//            choice = userResponse.getInt("Enter a choice...");
+//            switch (choice) {
+//                case (1): {
+//                    for (int i = 0; i < newMovie.length; i++) {
+//                        System.out.println(newMovie[i].getName() + " --- " + newMovie[i].getCategory());
+//continue;
+//                    }
+//                } case (2) :{
+//                    for (int i = 0; i < newMovie.length; i++) {
+//                        if (newMovie[i].getCategory() == "animated") {
+//                            System.out.println(newMovie[i].getName() + " --- " + newMovie[i].getCategory());
+//continue;
+//                        }
+//                    }
+//                } case  (3): {
+//                    for (int i = 0; i < newMovie.length; i++) {
+//                        if (newMovie[i].getCategory() == "drama") {
+//                            System.out.println(newMovie[i].getName() + " --- " + newMovie[i].getCategory());
+//continue;
+//                        }
+//                    }
+//                } case (4) :{
+//                    for (int i = 0; i < newMovie.length; i++) {
+//                        if (newMovie[i].getCategory() == "horror") {
+//                            System.out.println(newMovie[i].getName() + " --- " + newMovie[i].getCategory());
+//continue;
+//                        }
+//                    }
+//                } case (5) :{
+//                    for (int i = 0; i < newMovie.length; i++) {
+//                        if (newMovie[i].getCategory() == "sci-fi") {
+//                            System.out.println(newMovie[i].getName() + " --- " + newMovie[i].getCategory());
+//continue;
+//                        }
+//                    }
+//                } case (6): {
+//                    for (int i = 0; i < newMovie.length; i++) {
+//                        if (newMovie[i].getCategory() == "musical") {
+//                            System.out.println(newMovie[i].getName() + " --- " + newMovie[i].getCategory());
+//                            continue;
+//                        }
+//                    }
+//                } case (7): {
+//                    System.out.println("Good-bye!");
+//                    continueToRun = false;
+//                    break;
+//                }
+//            } } while (continueToRun);
+//    }
 
     public static void main(String[] args) {
         filterMovie();

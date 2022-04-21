bd1bb30fa534637270c401cc084c6207df79a497
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-21 12:16:53 -0500

    warmup

--- src/Warmup.java
+++ src/Warmup.java
@@ -4,13 +4,13 @@ public class Warmup {
     private static String backwards = "";
     public static boolean isPalindrome(String isIt) {
         for (int i = (isIt.length() - 1); i >= 0; --i) {
-           backwards = backwards + isIt.charAt(i);
+           backwards += isIt.charAt(i);
         }
         return isIt.equals(backwards);
     }
 
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome("radar"));
+        System.out.println(isPalindrome("abcba"));
     }
 }

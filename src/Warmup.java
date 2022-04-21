0d69374c4208b56bee41cc3bb11b83dae718ecae
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-21 10:08:26 -0500

    palindrome

--- src/Warmup.java
+++ src/Warmup.java
@@ -2,14 +2,12 @@ public class Warmup {
 
 
     private static String backwards = "";
-
     public static boolean isPalindrome(String isIt) {
         for (int i = (isIt.length() - 1); i >= 0; --i) {
             backwards = backwards + isIt.charAt(i);
         }
         return isIt.equals(backwards);
     }
-
     public static void main(String[] args) {
         System.out.println(isPalindrome("radar"));
     }

407add8260a573da9941a334699272efcb8d05a0
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-21 09:34:06 -0500

    palindrome warmup

--- src/Warmup.java
+++ src/Warmup.java
@@ -1,12 +1,28 @@
 public class Warmup {
 
+
+
+
+
+
+        public static boolean isPalindrome(String text) {
+            String clean = text.replaceAll("\\s+", "").toLowerCase();
+            int length = clean.length();
+            int forward = 0;
+            int backward = length - 1;
+            while (backward > forward) {
+                char forwardChar = clean.charAt(forward++);
+                char backwardChar = clean.charAt(backward--);
+                if (forwardChar != backwardChar)
+                    return false;
+            }
+            return true;
+        }
+
     public static void main(String[] args) {
-        boolean livesInCity = false;
-        boolean hasTransportation = true;
-        boolean canComeInPerson;
+
+        System.out.println(isPalindrome("radar"));
 
 
-        canComeInPerson = livesInCity && hasTransportation;
-        System.out.println("They can come in person is "+canComeInPerson);
     }
 }
